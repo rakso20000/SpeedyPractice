1d9dd02a48a1b5f24f6c2af82c9ef5d01db176e2
rakso20000 <rakso20000@gmail.com>
2020-10-16 23:36:39 +0200

    Initialize plugin

---
+++ SpeedyPractice/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+
+namespace SpeedyPractice {
+	
+	[Plugin(RuntimeOptions.SingleStartInit)]
+	public class Plugin {
+		
+		internal static Plugin instance { get; private set; }
+		internal static IPALogger log { get; private set; }
+		
+		[Init]
+		public Plugin(IPALogger logger) {
+			
+			instance = this;
+			log = logger;
+			
+		}
+		
+	}
+	
+}
