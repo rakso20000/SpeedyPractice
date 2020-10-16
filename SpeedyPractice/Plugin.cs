217c2f1f6ecd4db4b1e7e8b248408409d4382e7e
rakso20000 <rakso20000@gmail.com>
2020-10-17 00:41:42 +0200

    Increase maximum speed multiplier to 200%

--- SpeedyPractice/Plugin.cs
+++ SpeedyPractice/Plugin.cs
@@ -1,4 +1,7 @@
 ﻿using IPA;
+using IPA.Logging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using IPALogger = IPA.Logging.Logger;
 
 namespace SpeedyPractice {
@@ -14,6 +17,30 @@ namespace SpeedyPractice {
 			
 			instance = this;
 			log = logger;
+			
+		}
+		
+		[OnEnable]
+		public void OnEnable() {
+			
+			SceneManager.activeSceneChanged += OnSceneChanged;
+			
+		}
+		
+		[OnDisable]
+		public void OnDisable() {
+			
+			SceneManager.activeSceneChanged -= OnSceneChanged;
+			
+		}
+		
+		public void OnSceneChanged(Scene prevScene, Scene nextScene) {
+			
+			if (nextScene.name == "MenuViewControllers") {
+				
+				new GameObject("SpeedyLight").AddComponent<SliderController>();
+				
+			}
 			
 		}
 		
---
+++ SpeedyPractice/SliderController.cs
@@ -0,0 +1,29 @@
+﻿using HMUI;
+using TwitchFX;
+using UnityEngine;
+using Zenject;
+
+namespace SpeedyPractice {
+	
+	public class SliderController : MonoBehaviour {
+		
+		private PracticeViewController practiceView;
+		
+		[Inject]
+		public void Inject(PracticeViewController practiceView) {
+			
+			this.practiceView = practiceView;
+			
+		}
+		
+		public void Start() {
+			
+			PercentSlider slider = Helper.GetValue<PercentSlider>(practiceView, "_speedSlider");
+			
+			slider.maxValue = 2f;
+			
+		}
+		
+	}
+	
+}
