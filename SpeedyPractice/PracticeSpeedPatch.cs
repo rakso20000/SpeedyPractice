60a06a359e3566afb8edbbb5ff0ccffbe0294a78
Kinsi <kinsi55@users.noreply.github.com>
2022-02-17 19:37:40 +0100

    Update for 1.18+ - Changed to Harmonypatch implementation

---
+++ SpeedyPractice/PracticeSpeedPatch.cs
@@ -0,0 +1,26 @@
+﻿using HMUI;
+using UnityEngine;
+using HarmonyLib;
+using System;
+
+namespace SpeedyPractice {
+	[HarmonyPatch(typeof(PracticeViewController), "DidActivate")]
+	static class PracticeSpeedPatch {
+		static void Postfix(bool firstActivation, PercentSlider ____speedSlider) {
+			if(!firstActivation)
+				return;
+
+			int minSpeed = PluginConfig.instance.minSpeed;
+			int maxSpeed = PluginConfig.instance.maxSpeed;
+			int stepSize = Math.Max(1, PluginConfig.instance.stepSize);
+
+			maxSpeed -= (maxSpeed - minSpeed) % stepSize;
+
+			____speedSlider.minValue = minSpeed / 100f;
+			____speedSlider.maxValue = maxSpeed / 100f;
+			____speedSlider.numberOfSteps = (maxSpeed - minSpeed) / stepSize + 1;
+
+			____speedSlider.value = Math.Max(____speedSlider.minValue, 1);
+		}
+	}
+}
